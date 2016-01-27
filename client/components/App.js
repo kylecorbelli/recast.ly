ea05671b8ac558de8d4f3c536253c4eb1d03ecfe
pacificLiving <kyle.corbelli@gmail.com>
2016-01-27 15:54:31 -0800

    Refactor App using ES6 classes

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,25 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer mainVideo={exampleVideoData}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList theVideos={exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideoIndex: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer mainVideo={exampleVideoData}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList theVideos={exampleVideoData}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
