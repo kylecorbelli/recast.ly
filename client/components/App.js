720a1fe0964a868ec3843c0d5ff61cfe2f6cf925
pacificLiving <kyle.corbelli@gmail.com>
2016-01-27 15:48:15 -0800

    Pass a video to the main VideoPlayer component

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer mainVideo={exampleVideoData}/>
     </div>
     <div className="col-md-5">
       <VideoList theVideos={exampleVideoData}/>
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,13 +1,17 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+var VideoPlayer = (props) => {
+  console.log(props);
+  var videoLink = `https://www.youtube.com/embed/${props.mainVideo[0].id.videoId}?autoplay=1`;
+  return (
+    <div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={videoLink} allowFullScreen></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{props.mainVideo[0].snippet.title}</h3>
+        <div>{props.mainVideo[0].snippet.description}</div>
+      </div>
     </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
-);
+  )
+};
 
 window.VideoPlayer = VideoPlayer;
