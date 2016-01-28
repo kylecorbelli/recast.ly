271e9c72dc0db9bb8a4a0331e6249a4f8863b63f
pacificLiving <kyle.corbelli@gmail.com>
2016-01-27 18:30:07 -0800

    Attempt AJAX call

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log('in the function');
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/videos',
+    type: 'GET',
+    contentType: 'application/json',
+    data: {
+      key: YOUTUBE_API_KEY
+    },
+    success: function(data) {
+      // callback(data);
+      console.log('success!');
+    },
+    error: function(err) {
+      console.error('no good!', err.responseText);
+    }
+  });
 };
+
+searchYouTube();
 
 window.searchYouTube = searchYouTube;
